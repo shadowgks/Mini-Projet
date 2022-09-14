14367965af34a7d4f896d758cb2c1cff712679e1
saad moumou <saad.moumou.coder@gmail.com>
2022-09-14 22:00:10 +0100

    Create Function printIntro() which prints a fun intro message for the console race

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Structer Race declararion
+//Structuer Race declararion
 struct Race {
 //Members
 	int numberOfLaps;
@@ -10,17 +10,25 @@ struct Race {
 	char RaceCar[100];
 };
 
-//Structer Race declararion
+//Structuer Race declararion
 struct RaceCar  {
 //Members
 	char driverName[100];
 	char raceCarColor[100];
 	int totalLapTime;
 };
+
+//Create Function printIntro();
+void printIntro(){
+    printf("Bienvenue à notre événement principal, les fans de course numérique !\n");
+    printf("J'espère que tout le monde a pris son goûter car nous allons commencer !\n");
+}
 int main()
 {
 	struct Race R1;
 	struct RaceCar RC1;
 
+
+	printIntro();
     return 0;
 }
