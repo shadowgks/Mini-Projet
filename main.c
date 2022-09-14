4010639c0b00612535a4674f4033ea98b0c85a81
saad moumou <saad.moumou.coder@gmail.com>
2022-09-14 21:54:49 +0100

    Create section RaceCar

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Structer declararion
+//Structer Race declararion
 struct Race {
 //Members
 	int numberOfLaps;
@@ -10,9 +10,17 @@ struct Race {
 	char RaceCar[100];
 };
 
+//Structer Race declararion
+struct RaceCar  {
+//Members
+	char driverName[100];
+	char raceCarColor[100];
+	int totalLapTime;
+};
 int main()
 {
 	struct Race R1;
+	struct RaceCar RC1;
 
     return 0;
 }
