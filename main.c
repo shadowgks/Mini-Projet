5eb73aec2154732bff5f6069ed823a5807f88c97
saad moumou <saad.moumou.coder@gmail.com>
2022-09-14 21:13:20 +0100

    Add to structure Race

--- main.c
+++ main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Structer declararion
+struct Race {
+//Members
+	int numberOfLaps;
+	int currentLap;
+	char firstPlaceDriverName[100];
+	char RaceCar[100];
+};
+
 int main()
 {
-    printf("Hello world!\n");
+	struct Race R1;
+
     return 0;
 }
