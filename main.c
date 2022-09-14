01a19b8f52e479549af7b2fef3c042bbecd4b2fe
saad moumou <saad.moumou.coder@gmail.com>
2022-09-14 22:45:03 +0100

    fixed Some Things

--- main.c
+++ main.c
@@ -6,35 +6,24 @@ struct Race {
 //Members
 	int numberOfLaps;
 	int currentLap;
-	char firstPlaceDriverName[100];
-	char RaceCar[100];
+	char firstPlaceDriverName[20];
+	char firstPlaceRaceCarColor[20];
 };
 
-//Structuer Race declararion
+//Structuer RaceCar declararion
 struct RaceCar  {
 //Members
-	char driverName[100];
-	char raceCarColor[100];
+	char driverName[20];
+	char raceCarColor[20];
 	int totalLapTime;
 };
 
-//Create Function printIntro();
-void printIntro(){
-    printf("Bienvenue à notre événement principal, les fans de course numérique !\n");
-    printf("J'espère que tout le monde a pris son goûter car nous allons commencer !\n");
-}
+void printIntro();
+void printCountDown();
 
-//Create Function printCountDown();
-void printCountDown(){
-    printf("Coureurs prêts ! Dans...\n");
-    //Count Down
-    int i=5;
-    while(i > 1){
-        printf("%d\n",i);
-        i--;
-    }
-    printf("Course !\n");
-}
+
+//Create Function printFirstPlaceAfterLap();
+
 int main()
 {
 	struct Race R1;
@@ -44,4 +33,22 @@ int main()
 	printIntro();
 	printCountDown();
     return 0;
+}
+
+//Create Function printIntro();
+void printIntro(){
+    printf("Bienvenue à notre événement principal, les fans de course numérique !\n");
+    printf("J'espère que tout le monde a pris son goûter car nous allons commencer !\n");
+    printf("---------------------------------------------------------------------------\n");
+}
+//Create Function printCountDown();
+void printCountDown(){
+    printf("\nCoureurs prêts ! Dans...\n");
+    //Count Down
+    int i=5;
+    while(i >= 1){
+        printf("%d\n",i);
+        i--;
+    }
+    printf("Course !\n");
 }
