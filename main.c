e45a1dcf97b2f09cf2a4ece480e2e882d8d2a180
saad moumou <saad.moumou.coder@gmail.com>
2022-09-15 10:54:50 +0100

    Create Function printFirstPlaceAfterLap()

--- main.c
+++ main.c
@@ -1,49 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
-//Structuer Race declararion
-struct Race {
+// Structures section
+struct Race{
 //Members
 	int numberOfLaps;
 	int currentLap;
 	char firstPlaceDriverName[20];
 	char firstPlaceRaceCarColor[20];
 };
-
-//Structuer RaceCar declararion
-struct RaceCar  {
+struct RaceCar{
 //Members
 	char driverName[20];
 	char raceCarColor[20];
 	int totalLapTime;
 };
 
-void printIntro();
-void printCountDown();
-
-
-//Create Function printFirstPlaceAfterLap();
-
-int main()
-{
-	struct Race R1;
-	struct RaceCar RC1;
-
-
-	printIntro();
-	printCountDown();
-    return 0;
-}
-
-//Create Function printIntro();
+// Print functions section
 void printIntro(){
-    printf("Bienvenue à notre événement principal, les fans de course numérique !\n");
-    printf("J'espère que tout le monde a pris son goûter car nous allons commencer !\n");
+    printf("Bienvenue a notre evenement principal, les fans de course numerique !\n");
+    printf("J'espere que tout le monde a pris son gouter car nous allons commencer !\n");
     printf("---------------------------------------------------------------------------\n");
 }
-//Create Function printCountDown();
 void printCountDown(){
-    printf("\nCoureurs prêts ! Dans...\n");
+    printf("\nCoureurs prets ! Dans...\n");
     //Count Down
     int i=5;
     while(i >= 1){
@@ -52,3 +34,24 @@ void printCountDown(){
     }
     printf("Course !\n");
 }
+
+// Logic functions section
+void printFirstPlaceAfterLap(struct Race race){
+    printf("Apres le tour numero %d \nLa premiere place est occupee par: %s dans la voiture de course %s",race.numberOfLaps,race.firstPlaceDriverName,race.firstPlaceRaceCarColor);
+
+}
+int main()
+{
+    srand(time(0));
+	struct Race R1 = {1,2,"Saad","red"};
+	struct Race R2 = {1,2,"Nouhaila","blue"};
+	struct RaceCar RC1;
+
+
+	printIntro();
+	printCountDown();
+	printFirstPlaceAfterLap(R1)
+
+    return 0;
+}
+
